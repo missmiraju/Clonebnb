52d0e8df22bd10c082d0fa6b0427e9bf5cf17f8f
Oliver Stofer <oliver@stofer-foletti.ch>
2023-05-30 14:01:37 +0200

    created user and userTag models including relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :user_tags
+  has_many :tags, through: :user_tags
+
   # TODO: add validators
   # TODO: add new fields (first_name last_name username desctription profile_picture_url age:date gender)
   # to strong params in User controller (which might be called something else because of devise)
