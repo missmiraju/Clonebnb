e607f239cb793d2651ab95b61f594c30ab41b079
Oliver Stofer <oliver@stofer-foletti.ch>
2023-05-30 16:50:17 +0200

    iterate over cards, add seed for admin, tags, and the admin's userTags

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# add the tags
+tags = %w[Painting Singing Dancing Photography Running Weightlifting Politics Sci-Fi Environmentalism Yoga Winter sports Partying Reading Parenting Computers]
+tags << "Dog lover"
+tags << "Cat lover"
+tags << "Film & TV"
+tags << "Playing an instrument"
+
+tags.each do |tag|
+  Tag.create(title: tag)
+end
+
+# add the admin
 User.create(email: "admin@admin.com", password: "123456",
             first_name: "admin", last_name: "Meier",
             username: "__TheAdmin__", description: "I am the admin.",
             profile_picture_url: "https://thumbs.dreamstime.com/z/happy-man-okay-sign-portrait-white-background-showing-31418338.jpg",
             age: "2000-01-01", gender: "No, thanks")
+admin = User.first
+
+tags_list = Tag.all.last(20)
+3.times do
+  utag = UserTag.new
+  utag.user_id = admin.id
+  utag.tag = tags_list.sample
+  puts "user: #{utag.user_id}"
+  puts "tag:  #{utag.tag_id}"
+  utag.save
+end
