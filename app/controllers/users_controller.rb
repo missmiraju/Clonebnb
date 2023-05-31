0cf6cefa3f580534c15280fa3ec7e541666baaaf
Sasha Scherrer <sasha.burkhardt@hotmail.fr>
2023-05-31 14:37:24 +0200

    show profile part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
     @num_tags = 3 # define how many tags should be displayed on the initial page
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
