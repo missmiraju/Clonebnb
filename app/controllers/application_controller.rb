72819ecb88e98a1fe05691348b9a3417602998b2
Oliver Stofer <oliver@stofer-foletti.ch>
2023-05-31 14:33:22 +0200

    added pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user! # has to be logged in
+  include Pundit::Authorization
 
+  # @oli: past me here: you may need to change the following two lines
+  # later on if the pages where the authorisation is incorrect
+  # and/or move them to a different file
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,11 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+    # def index?
+    #   true
+    # end
+  end
+end
