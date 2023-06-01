6942e689032b78c9d163396d02fb96422dc0fd44
Oliver Stofer <oliver@stofer-foletti.ch>
2023-06-01 15:38:52 +0200

    mmmmmmmmm

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :meetups
   has_many :user_tags
   has_many :tags, through: :user_tags
   has_one_attached :photo
