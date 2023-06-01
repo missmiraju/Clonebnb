14688c3111ac3213fb515251aa50ab00d9785aa9
Oliver Stofer <oliver@stofer-foletti.ch>
2023-06-01 16:36:16 +0200

    im a teapot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
 
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    # @users = User.all.reject{ |user| user.meetups.empty? }
-    @users = policy_scope(User)
+    # @users = User.all
+    @users_with = User.all
+    @users_with = policy_scope(User).reject { |user| user.meetups.first.nil? }
+    # @users_without = User.all.select { |user| user.meetups.first.nil? }
+    # @users_without = policy_scope(User)
     @num_tags = 3 # define how many tags should be displayed on the initial page
   end
 
